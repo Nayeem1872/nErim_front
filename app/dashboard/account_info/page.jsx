c1dda4fb4196ed2c8f92e0c8e442a2519bb10461
nayeem1872 <nayeem.islam.1872@gmail.com>
2024-07-01 13:16:37 +0600

    skeleton added in account info

--- app/dashboard/account_info/page.jsx
+++ app/dashboard/account_info/page.jsx
@@ -1,14 +1,15 @@
-"use client";
-import styles from "./style.module.css";
-import { Row, Col, Card, Descriptions } from "antd";
+"use client"
+import { Skeleton, Row, Col, Card, Descriptions } from "antd";
 import { useTranslation } from "react-i18next";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import styles from "./style.module.css";
 
 function Account() {
   const { t } = useTranslation();
 
   const [dataSourceQuery, setDataSourceQuery] = useState([]);
+  const [loading, setLoading] = useState(true); // State for loading indicator
 
   useEffect(() => {
     const fetchData = async () => {
@@ -21,125 +22,16 @@ function Account() {
           },
         });
         setDataSourceQuery(response.data.data);
-      } catch (error) {}
+        setLoading(false); // Set loading to false once data is fetched
+      } catch (error) {
+        setLoading(false); // Set loading to false on error
+      }
     };
 
     fetchData();
   }, []);
 
   return (
-    // <>
-    //   <Breadcrumb style={{ padding: "10px" }}>
-    //     <Breadcrumb.Item>
-    //       <a
-    //         onClick={() => {
-    //           router.push(`/dashboard`);
-    //         }}
-    //         style={{ display: "flex", alignItems: "center", gap: "5px" }}
-    //       >
-    //         <LayoutDashboard
-    //           style={{ fontSize: "20px", marginBottom: "2px" }}
-    //           color="#0D85D8"
-    //         />
-    //       </a>
-    //     </Breadcrumb.Item>
-    //     <Breadcrumb.Item style={{ marginBottom: "20px" }}>
-    //       <div >
-    //         <span style={{ fontSize: "15px",color: "gray"  }}>{t("side_navbar.account_info")}</span>
-    //       </div>
-    //     </Breadcrumb.Item>
-    //   </Breadcrumb>
-    //   {/* <div className={styles.profile}> */}
-    //   <img
-    //     src="/image/Account.svg"
-    //     alt="User Image"
-    //     style={{
-    //       position: "relative",
-    //       width: "860px",
-    //       height: "550px",
-    //       marginLeft: "-20px",
-    //       marginTop:"0px",
-    //       top: "10",
-    //       left: "0",
-    //     }}
-    //   />
-    //   {/* </div> */}
-
-    //   <Row className={styles.middle} gutter={[24, 0]}>
-    //     <Col span={12} offset={6} className="mb-24">
-    //       <Card
-    //         bordered={true}
-    //         title={
-    //           <h3 className={`${styles.cardTitle} font-semibold m-0`}>
-    //             {t("account_info.ProfileInformation")}
-    //           </h3>
-    //         }
-    //         className={`${styles.cardCustom} header-solid h-full card-profile-information`}
-    //         style={{
-    //           position: "absolute",
-    //           top: "0",
-    //           left: "50%",
-    //           transform: "translate(-50%, -100%)",
-    //           marginTop:"-50px"
-    //         }}
-    //       >
-    //         {/* <hr className="my-25" /> */}
-    //         <Descriptions title="" style={{ marginTop: "20px",textAlign: "center"  }}>
-    //           <Descriptions.Item
-    //             label={t("account_info.OrgName")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //             style={{ textAlign: "center" }}
-    //           >
-    //             {dataSourceQuery.orgName}
-    //           </Descriptions.Item>
-    //           <Descriptions.Item
-    //             label={t("account_info.Subscription")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //           >
-    //             {dataSourceQuery.subscription}
-    //           </Descriptions.Item>
-    //           <Descriptions.Item
-    //             label={t("account_info.UserCount")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //           >
-    //             {dataSourceQuery.userCount}
-    //           </Descriptions.Item>
-    //           <Descriptions.Item
-    //             label={t("account_info.StartDate")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //           >
-    //             {dataSourceQuery.startDate}
-    //           </Descriptions.Item>
-    //           <Descriptions.Item
-    //             label={t("account_info.EndDate")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //           >
-    //             {dataSourceQuery.endDate}
-    //           </Descriptions.Item>
-    //           <Descriptions.Item
-    //             label={t("account_info.ModelName")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //           >
-    //             {dataSourceQuery.modelName}
-    //           </Descriptions.Item>
-    //           <Descriptions.Item
-    //             label={t("account_info.Status")}
-    //             span={3}
-    //             className={styles.descriptionItem}
-    //           >
-    //             {dataSourceQuery.status}
-    //           </Descriptions.Item>
-    //         </Descriptions>
-    //       </Card>
-    //     </Col>
-    //   </Row>
-    // </>
     <>
       <div className={styles.profile}>
         <img
@@ -166,71 +58,75 @@ function Account() {
             className={`${styles.cardCustom} header-solid h-full card-profile-information`}
             style={{ paddingTop: 0, paddingBottom: 16 }}
           >
-            <Row gutter={16}>
-              <Col xs={24} sm={12}>
-                <Descriptions title="" style={{ marginTop: "20px" }}>
-                  <Descriptions.Item
-                    label={t("account_info.OrgName")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.orgName}
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label={t("account_info.Subscription")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.subscription}
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label={t("account_info.UserCount")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.userCount}
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label={t("account_info.StartDate")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.startDate}
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label={t("account_info.EndDate")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.endDate}
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label={t("account_info.ModelName")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.modelName}
-                  </Descriptions.Item>
-                  <Descriptions.Item
-                    label={t("account_info.Status")}
-                    span={3}
-                    className={styles.descriptionItem}
-                  >
-                    {dataSourceQuery.status}
-                  </Descriptions.Item>
-                </Descriptions>
-              </Col>
-              <Col xs={24} sm={12}>
-                <img
-                  src="/image/account.svg"
-                  alt="Description of image"
-                  style={{
-                    width: "100%",
-                    height: "300px",
-                  }}
-                />
-              </Col>
-            </Row>
+            {loading ? ( // Conditionally render skeleton loader while loading
+              <Skeleton active paragraph={{ rows: 7 }} />
+            ) : (
+              <Row gutter={16}>
+                <Col xs={24} sm={12}>
+                  <Descriptions title="" style={{ marginTop: "20px" }}>
+                    <Descriptions.Item
+                      label={t("account_info.OrgName")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.orgName}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={t("account_info.Subscription")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.subscription}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={t("account_info.UserCount")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.userCount}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={t("account_info.StartDate")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.startDate}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={t("account_info.EndDate")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.endDate}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={t("account_info.ModelName")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.modelName}
+                    </Descriptions.Item>
+                    <Descriptions.Item
+                      label={t("account_info.Status")}
+                      span={3}
+                      className={styles.descriptionItem}
+                    >
+                      {dataSourceQuery.status}
+                    </Descriptions.Item>
+                  </Descriptions>
+                </Col>
+                <Col xs={24} sm={12}>
+                  <img
+                    src="/image/account.svg"
+                    alt="Description of image"
+                    style={{
+                      width: "100%",
+                      height: "300px",
+                    }}
+                  />
+                </Col>
+              </Row>
+            )}
           </Card>
         </Col>
       </Row>
